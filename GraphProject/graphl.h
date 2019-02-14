c5d145b6430d970df0fbf44c09ece5aa4c49d41d
Chyanne Haugen <chyhaugen@gmail.com>
2019-02-13 20:54:59 -0800

    created functions heading and data members for a graph that uses an adjacency list

--- GraphProject/graphl.h
+++ GraphProject/graphl.h
@@ -1,6 +1,35 @@
 #pragma once
+#include "stdafx.h"
+
+const int MAXNODES = 101; //per assignment instructions
+
+struct EdgeNode; //forward reference for the compiler
+
+struct GraphNode
+{
+	EdgeNode *edgeHead; //head of the list of edges
+	NodeData *data;  //data information about each node
+	bool visited;
+};
+
+struct EdgeNode
+{
+	int adjGraphNode; //subscript of the adjacent graph node
+	EdgeNode *nextEdge;
+};
 
 class GraphL
 {
+private:
+	GraphNode *adjacencyList[MAXNODES];
+
+public:
+	GraphL();
+
+
+
+
+
+	~GraphL();
 
 };
